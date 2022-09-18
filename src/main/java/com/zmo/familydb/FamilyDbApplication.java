37e54fb0a337e63a177f43baa7e9ccaa1227bc68
Zmo1914 <emil.dinev@hotmail.co.uk>
2022-09-18 22:00:38 +0300

    Initial commit

---
+++ src/main/java/com/zmo/familydb/FamilyDbApplication.java
@@ -0,0 +1,13 @@
+package com.zmo.familydb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamilyDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FamilyDbApplication.class, args);
+	}
+
+}
